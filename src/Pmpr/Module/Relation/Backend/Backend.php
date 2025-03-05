646647578d2c622b90ac0fe2c9f0f9fb72a3fe83
pmpr.dev <pmpr.dev@gmail.com>
2025-03-05 11:50:21 +0000

    Obfuscated

--- src/Pmpr/Module/Relation/Backend/Backend.php
+++ src/Pmpr/Module/Relation/Backend/Backend.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             677fc57c5720c             |
+    |             67c83a4ea8d92             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Relation\Backend; use Pmpr\Module\Relation\Container; class Backend extends Container { }
--- src/Pmpr/Module/Relation/Setting.php
+++ src/Pmpr/Module/Relation/Setting.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             677fc57c5720c             |
+    |             67c83a4ea8d92             |
     |_______________________________________|
 */
- namespace Pmpr\Module\Relation; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Setting as BaseClass; use Pmpr\Common\Foundation\Interfaces\Constants; class Setting extends BaseClass { public function wyyuauosmqoeucmg() { $this->title = __("\x52\145\154\141\x74\x69\x6f\156", PR__MDL__RELATION); $this->igiywquyccyiaucw(Constants::ysgwugcqguggmigq, __("\122\145\x6c\x61\x74\x69\157\x6e\40\123\145\164\164\x69\x6e\147", PR__MDL__RELATION)); } }
+ namespace Pmpr\Module\Relation; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Setting as BaseClass; use Pmpr\Common\Foundation\Interfaces\Constants; class Setting extends BaseClass { public function wyyuauosmqoeucmg() { $this->title = __('Relation', PR__MDL__RELATION); $this->igiywquyccyiaucw(Constants::ysgwugcqguggmigq, __('Relation Setting', PR__MDL__RELATION)); } }
