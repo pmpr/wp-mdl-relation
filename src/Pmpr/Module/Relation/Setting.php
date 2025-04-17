e8e1ea5816a59086a2df125d901882885b61db44
pmpr.dev <pmpr.dev@gmail.com>
2025-04-17 13:48:18 +0000

    Obfuscated

--- src/Pmpr/Module/Relation/Setting.php
+++ src/Pmpr/Module/Relation/Setting.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             67c83a4ea8d92             |
+    |             68010672ee494             |
     |_______________________________________|
 */
- namespace Pmpr\Module\Relation; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Setting as BaseClass; use Pmpr\Common\Foundation\Interfaces\Constants; class Setting extends BaseClass { public function wyyuauosmqoeucmg() { $this->title = __('Relation', PR__MDL__RELATION); $this->igiywquyccyiaucw(Constants::ysgwugcqguggmigq, __('Relation Setting', PR__MDL__RELATION)); } }
+ namespace Pmpr\Module\Relation; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Setting as BaseClass; use Pmpr\Common\Foundation\Interfaces\Constants; class Setting extends BaseClass { public function qiccuiwooiquycsg() { $this->gswweykyogmsyawy(__('Relation', PR__MDL__RELATION))->igiywquyccyiaucw(Constants::ysgwugcqguggmigq, __('Relation Setting', PR__MDL__RELATION)); parent::qiccuiwooiquycsg(); } }
